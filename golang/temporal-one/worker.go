21f49c8a296a9e3bf98375a2101f27f270c73366
Rafael Algara <ralgara@gmail.com>
2024-01-06 00:47:05 -0500

    Better logging in Go filter worker

--- golang/temporal-one/worker.go
+++ golang/temporal-one/worker.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/client"
@@ -11,20 +12,27 @@ import (
 )
 
 func main() {
+	log.Println("Starting Golang filter worker [v0.1]")
+	hostPort := os.Getenv("TEMPORAL_SERVER")
+	log.Printf("Connecting to server [%s]\n", hostPort)
 	temporalClient, err := client.Dial(client.Options{
-		HostPort:  client.DefaultHostPort,
+		HostPort:  hostPort,
 		Namespace: "default",
 	})
 	if err != nil {
 		log.Fatalln("Unable to create Temporal Client", err)
 	}
 	defer temporalClient.Close()
-	w := worker.New(temporalClient, "wikipedia-filter",
+	queue := "wikipedia-filter"
+	log.Printf("Create worker [queue:%s]\n", queue)
+	w := worker.New(temporalClient, queue,
 		worker.Options{
 			DisableWorkflowWorker: true,
 		},
 	)
 
+	activityName := "filter_articles"
+	log.Printf("Register activity [%s]\n", activityName)
 	w.RegisterActivityWithOptions(
 		filter_articles,
 		activity.RegisterOptions{
@@ -32,10 +40,12 @@ func main() {
 		},
 	)
 
+	log.Printf("Running loop")
 	err = w.Run(worker.InterruptCh())
 	if err != nil {
 		log.Fatalln("Unable to start Worker", err)
 	}
+	log.Printf("Exiting")
 }
 
 type ArticleMetadata struct {
